80abcce8cece03edb199689ee66c31ededb879d3
Abhi Patel <abhi.d@simformsolutions.com>
2023-06-27 16:27:48 +0530

    add : test1 which are on routes

--- Practical_10/App_Start/RouteConfig.cs
+++ Practical_10/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Practical_10
@@ -12,6 +8,13 @@ namespace Practical_10
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				name: "Home",
+				url: "Home/{name}",
+				defaults: new { controller = "Home", action = "Index" }
+			);
+
 
 			routes.MapRoute(
 				name: "Default",
--- Practical_10/Controllers/HomeController.cs
+++ Practical_10/Controllers/HomeController.cs
@@ -2,10 +2,16 @@
 
 namespace Practical_10.Controllers
 {
+	[Route("Home")]
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
+		[Route("Home/{name}")]
+		public ActionResult Index(string name)
 		{
+			if (!string.IsNullOrEmpty(name))
+			{
+				ViewBag.Name = name;
+			}
 			return View();
 		}
 
