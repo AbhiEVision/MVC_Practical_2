c55aa47647d2d872e3e302887aa88079925fb6de
Abhi Patel <abhi.d@simformsolutions.com>
2023-06-29 15:26:51 +0530

    test 3 added which are about caching in asp .net mvc

---
+++ OutputCacheTest/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace OutputCacheTest.Controllers
+{
+	public class HomeController : Controller
+	{
+		[OutputCache(Duration = 300, Location = System.Web.UI.OutputCacheLocation.ServerAndClient)]
+		public ActionResult Index()
+		{
+			ViewBag.Data = DateTime.Now.ToString();
+			return View();
+		}
+
+
+	}
+}
