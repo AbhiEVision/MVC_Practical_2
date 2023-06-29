9a53bc37d3bf605fe8165578cbbdc82f0060c7ed
Abhi Patel <abhi.d@simformsolutions.com>
2023-06-29 15:24:38 +0530

    add : test 4 added which are about the global error handaling

---
+++ ExceptionFilter/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ExceptionFilter.Controllers
+{
+
+
+	public class HomeController : Controller
+	{
+		[HandleError(ExceptionType = typeof(DivideByZeroException), View = "DivideByZero")]
+		public ActionResult Index()
+		{
+			int a = 0;
+			int c = 0;
+			int b = c / a;
+			ViewBag.b = b;
+
+
+			return View();
+		}
+
+
+	}
+}
